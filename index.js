2fe6e2cd6ade1645d367a229f17fce8919074a3c
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-26 23:21:37 +0600

    created courses data and sending to the server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const courses = require('./courses.json')
 const port = 5000;
 
 app.get('/', (req, res)=>{
     res.send('Server is running...')
+})
+
+app.get('/courses',(req, res)=>{
+    res.send(courses);
 })
 
 app.listen(port, ()=>{
