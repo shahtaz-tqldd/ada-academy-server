cd4617e5f26ab02f52e3262a07268c60c3870458
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-27 10:49:07 +0600

    deployed the server to the vercel

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const courses = require('./courses.json')
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('Server is running...')
