e4d01eb03084767e9146b397d245d522301cf011
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-27 18:15:07 +0600

    final commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const courses = require('./courses.json')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 
 app.use(cors());
 
